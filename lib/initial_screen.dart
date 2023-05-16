1310e4b0444ff8ac856c0248be8f02344a094ee5
yujinkim1 <yujin7621@me.com>
2023-05-17 00:48:58 +0900

    add(#3): Initial screen

---
+++ lib/initial_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("권한 요청 통과 테스트 스크린"),
+      ),
+    );
+  }
+}
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  late CameraController cameraController;
+  late List<CameraDescription> cameras;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeCamera();
+  }
+
+  Future<void> initializeCamera() async {
+    cameras = await availableCameras();
+    if (cameras.isEmpty) {
+      return;
+    } else {
+      //TODO: 화면 전환 등 다음 작업을 위한 내용이 추가되어야함
+    }
+  }
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
