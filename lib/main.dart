f6d0d9070618db7a35f01827c72fecfaa7be1c9a
yujinkim1 <yujin7621@me.com>
2023-05-21 21:44:28 +0900

    modified: Fetch the available cameras before initialized main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
+import 'dart:developer';
+import 'package:blindover_flutter/preview_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    log("카메라 연결에 실패했습니다. 오류:[$e]");
+  }
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: PreviewScreen(),
       ),
     );
   }
