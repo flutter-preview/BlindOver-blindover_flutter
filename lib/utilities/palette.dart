c213bd109e0b1d754ea1ca216ec9d70b7e389539
yujinkim1 <yujin7621@me.com>
2023-06-01 13:18:22 +0900

    add(colors): Default color set

---
+++ lib/utilities/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+///- 내부에서 [`Color`] 속성에 할당하기 위해 기본 색상을 구성하고 있는 클래스입니다.
+class Palette {
+  static const surface = Color(0xffF5F5F5);
+  static const onSurface = Color(0xff333333);
+  static const primary = Color(0xff333333);
+  static const secondary = Color(0xff916400);
+  static const secondaryContainer = Color(0xffFFC107);
+  static const tertiary = Color(0xffFF8C00);
+}
