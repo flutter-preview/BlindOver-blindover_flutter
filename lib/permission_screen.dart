b0b8c06e9c853fb14283e521336e2216e9b4ac5a
yujinkim1 <yujin7621@me.com>
2023-06-01 12:43:21 +0900

    add(route): Replacement screen as permission

--- lib/permission_screen.dart
+++ lib/permission_screen.dart
@@ -1,13 +1,15 @@
+import 'dart:io';
 import 'dart:developer';
 
-import 'package:blindover_flutter/utilities/palette.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:lottie/lottie.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-// import 'package:blindover_flutter/preview_screen.dart';
+import 'package:blindover_flutter/preview_screen.dart';
 import 'package:blindover_flutter/widgets/constraint_large_button.dart';
+import 'package:blindover_flutter/utilities/palette.dart';
 
 class PermissionScreen extends StatefulWidget {
   const PermissionScreen({Key? key}) : super(key: key);
@@ -17,28 +19,58 @@ class PermissionScreen extends StatefulWidget {
 }
 
 class _PermissionScreenState extends State<PermissionScreen> {
-  final String lottieCamera = "assets/lotties/camera.json";
+  ///- 사용자가 카메라 권한을 허용하고 카메라를 활성화할 수 있는지 확인하는 변수입니다.
+  ///- [`isEnabled = false`]인 경우 카메라 권한을 다시 받을 수 있도록 합니다.
+  ///- [`isEnabled = true`]인 경우 카메라를 생성하는 화면으로 바꿉니다.
+  late bool isEnabled;
+  late dynamic lottieCamera;
+
+  ///- permission_handler에서 [`Permission.camera.status`] 값을 비동기 처리합니다.
+  ///- [`cameraStatus`] 값에 따라 간단하게 상태를 분리합니다.
+  Future<bool> checkPermission() async {
+    var cameraStatus = await Permission.camera.status;
+
+    if (cameraStatus.isGranted) {
+      log("checkPermission: $cameraStatus");
+      return true;
+    } else if (cameraStatus.isDenied) {
+      log("checkPermission: $cameraStatus");
+      return false;
+    } else if (cameraStatus.isPermanentlyDenied) {
+      log("사용자 또는 시스템에서 잘못 선택되어 설정 앱으로 이동합니다.");
+      await openAppSettings();
+      return false;
+    }
+    return false;
+  }
 
   @override
   void initState() {
     super.initState();
   }
 
-  Future<bool> requestCameraPermission() async {
-    var cameraStatus = await Permission.camera.status;
+  @override
+  void didChangeDependencies() {
+    lottieCamera = Lottie.asset("assets/lotties/camera.json");
+    super.didChangeDependencies();
+  }
 
-    if (cameraStatus.isGranted) {
-      log("requestCameraPermission: $cameraStatus");
-      return true;
-    } else if (cameraStatus.isDenied) {
-      log("requestCameraPermission: $cameraStatus");
-      return false;
-    } else if (cameraStatus.isPermanentlyDenied) {
-      log("사용자 또는 시스템에서 잘못 선택된 경우 설정으로 바로 이동하도록 유도합니다.");
-      await openAppSettings();
-      return false;
+  void navigateScreen() {
+    if (Platform.isIOS) {
+      Navigator.pushReplacement(
+        context,
+        CupertinoPageRoute(
+          builder: (context) => const PreviewScreen(),
+        ),
+      );
+    } else if (Platform.isAndroid) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const PreviewScreen(),
+        ),
+      );
     }
-    return false;
   }
 
   @override
@@ -58,10 +90,10 @@ class _PermissionScreenState extends State<PermissionScreen> {
               children: [
                 const SizedBox(height: 50.0),
                 const HeaderContentWidget(
-                  words: "서비스를 사용하려면 카메라 권한을 허용해야 합니다.",
+                  words: "서비스를 사용하려면\n카메라 권한을 허용해야 합니다.",
                 ),
                 Expanded(
-                  child: Lottie.asset(lottieCamera),
+                  child: lottieCamera,
                 ),
                 const SizedBox(height: 50.0),
                 ConstraintLargeButton(
@@ -69,11 +101,11 @@ class _PermissionScreenState extends State<PermissionScreen> {
                   value: "허용하기",
                   onTap: () async {
                     await Permission.camera.request();
-                    await requestCameraPermission();
-                    if (requestCameraPermission == true) {
-                      log("true");
-                    } else if (requestCameraPermission == false) {
-                      log("false");
+                    isEnabled = await checkPermission();
+                    if (isEnabled) {
+                      navigateScreen();
+                    } else if (!isEnabled) {
+                      await checkPermission();
                     }
                   },
                 ),
@@ -104,8 +136,9 @@ class HeaderContentWidget extends StatelessWidget {
         words ?? "",
         semanticsLabel: words,
         style: const TextStyle(
-          fontSize: 25.0,
-          fontWeight: FontWeight.w300,
+          fontFamily: "Roboto",
+          fontSize: 30.0,
+          fontWeight: FontWeight.w500,
           color: Palette.onSurface,
         ),
       ),
