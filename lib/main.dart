3be27bc477bb53e7513795e264fdbfd99c4a226c
yujinkim1 <yujin7621@me.com>
2023-06-02 23:38:56 +0900

    modified(builder): Used future builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
 import 'dart:developer';
 
+import 'package:blindover_flutter/preview_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
 import 'package:blindover_flutter/permission_screen.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 List<CameraDescription> cameras = <CameraDescription>[];
+
+// var permissionStatus = Permission.camera.status.isGranted;
 
 Future<void> main() async {
   try {
@@ -30,23 +34,49 @@ class MainApp extends StatelessWidget {
     return Builder(
       /// Builder() 위젯의 `builder: [Widget Function(BuildContext)]` 인수는 null일 수 없습니다.
       builder: (context) {
-        /// 타겟 플랫폼이 iOS라면 쿠퍼티노 디자인의 scaffold를 빌드합니다.
-        if (Platform.isIOS) {
-          return const CupertinoApp(
-            home: CupertinoPageScaffold(
-              child: PermissionScreen(),
-            ),
-          );
+        // /// 타겟 플랫폼이 iOS라면 쿠퍼티노 디자인의 scaffold를 빌드합니다.
+        // if (Platform.isIOS) {
+        //   return CupertinoApp(
+        //     home: CupertinoPageScaffold(
+        //       child: permissionStatus == Permission.camera.status.isGranted
+        //           ? const PreviewScreen()
+        //           : const PermissionScreen(),
+        //     ),
+        //   );
 
-          /// 타겟 플랫폼이 AOS라면 머티리얼 디자인의 scaffold를 빌드합니다.
-        } else if (Platform.isAndroid) {
-          return const MaterialApp(
-            home: Scaffold(
-              body: PermissionScreen(),
-            ),
-          );
-        }
-        return const Scaffold();
+        //   /// 타겟 플랫폼이 AOS라면 머티리얼 디자인의 scaffold를 빌드합니다.
+        // } else if (Platform.isAndroid) {
+        //   return MaterialApp(
+        //     home: Scaffold(
+        //       body: permissionStatus == Permission.camera.status.isGranted
+        //           ? const PreviewScreen()
+        //           : const PermissionScreen(),
+        //     ),
+        //   );
+        // }
+        return MaterialApp(
+          home: FutureBuilder<PermissionStatus>(
+            future: Permission.camera.status,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                if (Platform.isIOS) {
+                  return const CupertinoActivityIndicator();
+                } else if (Platform.isAndroid) {
+                  return const CircularProgressIndicator();
+                }
+              } else if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.data!.isGranted) {
+                  return const PreviewScreen();
+                } else if (snapshot.data!.isDenied) {
+                  return const PermissionScreen();
+                } else if (snapshot.data!.isPermanentlyDenied) {
+                  return const PermissionScreen();
+                }
+              }
+              return const PermissionScreen();
+            },
+          ),
+        );
       },
     );
   }
