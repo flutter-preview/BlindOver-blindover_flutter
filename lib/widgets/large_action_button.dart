be6fbfe9668b36799fef1ad422fdadc58491d1b6
yujinkim1 <yujin7621@me.com>
2023-06-08 22:10:59 +0900

    modified: Child widget

--- lib/widgets/large_action_button.dart
+++ lib/widgets/large_action_button.dart
@@ -5,13 +5,12 @@ import 'package:flutter/material.dart';
 
 import 'package:blindover_flutter/utilities/palette.dart';
 
-///- `LageActionButton()` 위젯은 Stateless 위젯을 상속받습니다.
-///- Constraint 형식의 큰 버튼으로 디바이스에서 최대 크기로 나타납니다.
+/// [LageActionButton] 위젯은 사용자의 액션을 감지하고 처리할 수 있습니다.
 class LargeActionButton extends StatelessWidget {
-  ///- String 값을 할당하면, Semantic 위젯에서 이를 처리할 수 있습니다.
+  /// String 값을 할당하면, Semantic 위젯에서 이를 처리할 수 있습니다.
   final String label;
 
-  ///- String 값을 할당해서 버튼의 역할을 시각화합니다.
+  /// String 값을 할당해서 버튼의 역할을 시각화합니다.
   final String words;
 
   ///- 함수를 할당하면 사용자의 액션을 감지하고 처리할 수 있습니다.
@@ -34,21 +33,21 @@ class LargeActionButton extends StatelessWidget {
       excludeSemantics: false,
       label: label,
       onTap: onTap,
-      child: Platform.isIOS
-          ? CupertinoButton(
-              onPressed: onTap,
-              color: Palette.secondaryContainer,
-              child: Text(
-                words,
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
-            )
-          : ElevatedButton(
+      child: Platform.isAndroid
+          ? ElevatedButton(
               onPressed: onTap,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Palette.secondaryContainer,
                 elevation: 2.0,
               ),
+              child: Text(
+                words,
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+            )
+          : CupertinoButton(
+              onPressed: onTap,
+              color: Palette.secondaryContainer,
               child: Text(
                 words,
                 style: Theme.of(context).textTheme.displayMedium,
